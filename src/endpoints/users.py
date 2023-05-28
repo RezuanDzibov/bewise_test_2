611fb3d405c9a8a11772524900ca7fe7c2eb0d1b
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2023-05-28 22:47:10 +0300

    Add add_user endpoint

---
+++ src/endpoints/users.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from dependencies import get_session
+from schemas.users import UserInSchema, UserOutSchema
+from services.users import insert_user
+
+router = APIRouter()
+
+
+@router.post("", response_model=UserOutSchema)
+async def add_user(user: UserInSchema, session: AsyncSession = Depends(get_session)):
+    user = await insert_user(session=session, user_in_schema=user)
+    return user
