4d2d4f7dd7dba3372f1e9c2b6a208cb79c62d311
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2023-05-28 20:30:16 +0300

    Add settings.py

---
+++ src/core/settings.py
@@ -0,0 +1,35 @@
+from functools import lru_cache
+from pathlib import Path
+
+from pydantic import BaseSettings, PostgresDsn
+
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
+
+
+class Settings(BaseSettings):
+    PROJECT_NAME: str
+
+    DATABASE_ENGINE: str
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_PORT: str
+    POSTGRES_DATABASE: str
+    POSTGRES_HOST: str
+
+    @property
+    def SQLALCHEMY_DATABASE_URI(self) -> str:
+        return PostgresDsn.build(
+            scheme=self.DATABASE_ENGINE,
+            user=self.POSTGRES_USER,
+            password=self.POSTGRES_PASSWORD,
+            host=self.POSTGRES_HOST,
+            path=f"/{self.POSTGRES_DATABASE}",
+        )
+
+    class Config:
+        env_file = Path(f"{BASE_DIR}/.env")
+
+
+@lru_cache
+def get_settings() -> Settings:
+    return Settings()
