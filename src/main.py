4c656bd293e35086309f1d03f1798e1204283453
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2023-05-28 22:45:05 +0300

    Add redirect to docs

--- src/main.py
+++ src/main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from starlette.responses import RedirectResponse
 
 from core.settings import get_settings
 
 settings = get_settings()
 
 app = FastAPI(title=settings.PROJECT_NAME)
+app.include_router(router)
+
+
+@app.get("/")
+async def redirect_to_docs():
+    return RedirectResponse("/docs")
