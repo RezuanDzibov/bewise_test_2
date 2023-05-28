0f70e327bff8464ba38255c1050a71220e6f7665
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2023-05-28 22:46:44 +0300

    Add insert_user service

---
+++ src/services/users.py
@@ -0,0 +1,14 @@
+from sqlalchemy import insert
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from models import User
+from schemas.users import UserInSchema, UserOutSchema
+
+
+async def insert_user(session: AsyncSession, user_in_schema: UserInSchema) -> UserOutSchema:
+    statement = insert(User).values(user_in_schema.dict())
+    statement = statement.returning(User)
+    result = await session.execute(statement)
+    user_in_db = result.scalar()
+    user_out_schema = UserOutSchema(id=user_in_db.id, access_token=user_in_db.access_token)
+    return user_out_schema
