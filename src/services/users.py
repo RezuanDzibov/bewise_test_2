7233ca5ff886d6c2e1004a2170fb8a6d5ca0d5ef
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2023-05-29 15:40:58 +0300

    Commit add user

--- src/services/users.py
+++ src/services/users.py
@@ -9,6 +9,7 @@ async def insert_user(session: AsyncSession, user_in_schema: UserInSchema) -> Us
     statement = insert(User).values(user_in_schema.dict())
     statement = statement.returning(User)
     result = await session.execute(statement)
+    await session.commit()
     user_in_db = result.scalar()
     user_out_schema = UserOutSchema(id=user_in_db.id, access_token=user_in_db.access_token)
     return user_out_schema
