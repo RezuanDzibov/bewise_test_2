0c6baacbf3f3eadfb337c66d7d354c5836051159
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2023-05-29 15:41:29 +0300

    Format insert_user with black

--- src/services/users.py
+++ src/services/users.py
@@ -5,11 +5,15 @@ from models import User
 from schemas.users import UserInSchema, UserOutSchema
 
 
-async def insert_user(session: AsyncSession, user_in_schema: UserInSchema) -> UserOutSchema:
+async def insert_user(
+    session: AsyncSession, user_in_schema: UserInSchema
+) -> UserOutSchema:
     statement = insert(User).values(user_in_schema.dict())
     statement = statement.returning(User)
     result = await session.execute(statement)
     await session.commit()
     user_in_db = result.scalar()
-    user_out_schema = UserOutSchema(id=user_in_db.id, access_token=user_in_db.access_token)
+    user_out_schema = UserOutSchema(
+        id=user_in_db.id, access_token=user_in_db.access_token
+    )
     return user_out_schema
