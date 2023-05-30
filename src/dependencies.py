4feb2603943e0344c81f114bbddc2a2a0f708b94
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2023-05-30 22:05:09 +0300

    Refactor get_user dependency

--- src/dependencies.py
+++ src/dependencies.py
@@ -2,13 +2,12 @@ from typing import AsyncGenerator
 
 from fastapi import Depends, Security, HTTPException
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
-from pydantic import conint
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.db import SessionLocal
 from core.settings import get_settings
 from schemas.users import UserSchema
-from services.users import get_user_by_id_and_access_token
+from services.users import get_user_by_access_token
 
 settings = get_settings()
 bearer = HTTPBearer()
@@ -20,15 +19,13 @@ async def get_session() -> AsyncGenerator[AsyncSession, None]:
 
 
 async def get_user(
-    user_id: conint(ge=1),
     auth_creds: HTTPAuthorizationCredentials = Security(bearer),
     session: AsyncSession = Depends(get_session),
 ) -> UserSchema:
-    user = await get_user_by_id_and_access_token(
+    user = await get_user_by_access_token(
         session=session,
-        user_id=user_id,
         access_token=auth_creds.credentials,
     )
     if not user:
-        raise HTTPException(status_code=403, detail="User id or access token invalid")
+        raise HTTPException(status_code=404, detail="User not_found")
     return user
