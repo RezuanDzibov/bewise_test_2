b7c1d55e125f3fe0fa36827b13ae1f2c3fc10f15
Rezuan Dzibov <rezuan.dzbov@gmail.com>
2023-06-02 15:45:05 +0300

    insert_user return UserSchema

--- src/services/users.py
+++ src/services/users.py
@@ -2,21 +2,19 @@ from sqlalchemy import insert, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from models import User
-from schemas.users import UserInSchema, UserOutSchema, UserSchema
+from schemas.users import UserInSchema, UserSchema
 
 
 async def insert_user(
     session: AsyncSession, user_in_schema: UserInSchema
-) -> UserOutSchema:
+) -> UserSchema:
     statement = insert(User).values(user_in_schema.dict())
     statement = statement.returning(User)
     result = await session.execute(statement)
     await session.commit()
     user_in_db = result.scalar()
-    user_out_schema = UserOutSchema(
-        id=user_in_db.id, access_token=user_in_db.access_token
-    )
-    return user_out_schema
+    user_schema = UserSchema.from_orm(user_in_db)
+    return user_schema
 
 
 async def get_user_by_access_token(
